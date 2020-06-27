5e75b568a2092c09fe8f4f19d7974b8482a6f97b
Daniela Montenegro <dcmonten@espol.edu.ec>
2020-06-27 00:38:04 -0500

    Validador de sudoku con validacion en matrices. TODO: Fork para matar programa si dups se suma 1, señales para matar al programa con kill cuando dups ==1

--- prog3/sudoku/sudoku.c
+++ prog3/sudoku/sudoku.c
@@ -3,14 +3,19 @@
 #include <unistd.h>
 #include <pthread.h>
 #include <stdbool.h>
-#define NUM_THEADS 18
+#include<signal.h> 
 
-
+#define NUM_THEADS 27
+#define SIGQUIT 3 
 //Estructura que se envía como parámetro
 typedef struct {
 	int row;
 	int column;		
 } parameters;
+//mutex para escribir sobre la varible dups
+pthread_mutex_t mutexsum;
+
+int dups = 0;
 
 // Sudoku 9x9 resuelto, para que revise todos los casos
 int sudoku_resuelto[9][9] = {
@@ -37,13 +42,16 @@ int duplicado(int* lista);
 bool contains(int *values, size_t size, int value);
 
 int main() {	
+
     pthread_t hilos[NUM_THEADS];
     int index=0;
     int i,j;
-    //recorro la matriz 9x9, para los hilos que revisan filas y columnas
+    //mutex para aumentar el numero de duplicados correctamente en dups
+    pthread_mutex_init(&mutexsum, NULL);             
+    
     for (i=0;i<9;i++){
+        //creo los hilos con su parametro y varío el indice para leer filas y columnas
         parameters *dt_col = (parameters *) malloc(sizeof(parameters));	
-
         dt_col->row = 0;		
 		dt_col->column = i;
         pthread_create(&hilos[index++],NULL,revisarColumna,dt_col);
@@ -52,13 +60,35 @@ int main() {
         dt_row->row = i;		
 		dt_row->column = 0;
         pthread_create(&hilos[index++],NULL,revisarFila,dt_row);
+        //necesito otro for para enviar los datos de las matrices
+        for (int j = 0; j < 9; j++)
+        {
+            //solo necesito hacer cortes en índices multiplos de 3
+            if(j%3==0 && i%3==0){
+                parameters *dt_matrix = (parameters *) malloc(sizeof(parameters));	
+                dt_matrix->row = i;		
+                dt_matrix->column = j;
+                pthread_create(&hilos[index++],NULL,revisarMatriz,dt_matrix);
+            }
 
+        }
+        
     }
 
 	for (i = 0; i < NUM_THEADS; i++) {
 		pthread_join(hilos[i], NULL);			// Wait for all threads to finish
 	}
     
+    if (dups>0)
+    {
+        printf("Sudoku con duplicados\n");
+    }
+    else
+    {
+        printf("Sudoku resuelto correctamente\n");
+    }
+    
+    pthread_mutex_destroy(&mutexsum);
 	return EXIT_SUCCESS;
 
 }
@@ -73,12 +103,15 @@ void *revisarFila(void* param){
     //creo columnas
     for (size_t i = 0; i < 9; i++)
     {
-        fila[i]=sudoku_resuelto[col][i];   
+        fila[i]=sudoku_resuelto[row][i];   
     }
     //busco duplicados en la fila
     int dup=duplicado(fila);
     if (dup>0){
         printf("Numero duplicado en fila %d\n",row);
+        pthread_mutex_lock (&mutexsum);
+        dups++;
+        pthread_mutex_unlock (&mutexsum);
         pthread_exit(NULL); 
     }
     else
@@ -103,6 +136,9 @@ void *revisarColumna(void* param){
     int dup=duplicado(columna);
     if (dup>0){
         printf("Numero duplicado en columna %d\n",col);
+        pthread_mutex_lock (&mutexsum);
+        dups++;
+        pthread_mutex_unlock (&mutexsum);
         pthread_exit(NULL); 
     }
     else
@@ -111,12 +147,42 @@ void *revisarColumna(void* param){
     }
 }
 
-
+void *revisarMatriz(void* param){
+    parameters *params = (parameters*) param;
+	int i= params->row;
+	int j= params->column;
+    int matrix[9]={0,0,0,0,0,0,0,0,0};
+    int index=0;
+    //recorro la matriz 3x3 con el offset de i y j del for en main
+    for (int k = 0; k < 3; k++)
+    {
+        for (int l = 0; l < 3; l++)
+        {
+            matrix[index]=sudoku_resuelto[k+i][l+j];
+            index++;
+        }
+    }
+    //reviso duplicados en la lista
+    int dup=duplicado(matrix);
+    if (dup>0){
+        printf("Número duplicado en Matriz con inicio en columna %d fila %d\n",i,j);
+        pthread_mutex_lock (&mutexsum);
+        dups++;//escribo en dups si hay duplicados
+        pthread_mutex_unlock (&mutexsum);
+        pthread_exit(NULL); 
+    }
+    else
+    {
+        pthread_exit(NULL);
+    }
+    printf("\n----------\n");
+}
 //retorno el índice del duplicado
 int duplicado(int* lista)
 {
     for (size_t i = 0; i < 9; i++)
     {
+        
         if(contains(lista,i,lista[i])){
             //me salto el valor igual
             continue;
@@ -138,3 +204,4 @@ bool contains(int *values, size_t size, int value)
             return true;
     return false;
 }
+
