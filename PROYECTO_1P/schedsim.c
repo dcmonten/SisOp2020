0c5a7964e84975e6773c5b18c0042280f328b0f2
Daniela Montenegro <dcmonten@espol.edu.ec>
2020-07-19 23:22:20 -0500

    Validaciónd e argumentos completa

--- PROYECTO_1P/schedsim.c
+++ PROYECTO_1P/schedsim.c
@@ -4,7 +4,7 @@
 #include <unistd.h>
 #include <pthread.h>
 #include <stdbool.h>
-
+#include <ctype.h>
 //vars
 char scheds_list[3][5] = {"fcfs","sjf","rr"}; //tipos de planificadores
 //mutex para acceso al io
@@ -19,31 +19,72 @@ void sjf();
 void fcfs();
 //5. Busca el índice del scheduler usando el argumento 1
 int indexSched(char* sched);
+//6. llena las listas de arrival y burst con el archivo, retorna true si lo hizo satisfactoriamente,
+//false cuando la ruta del archivo es incorrecta.
+bool fillProcessQueues(char * file_path);
+
 //MAIN
 int main(int argc, char *argv[])
 {
-    pthread_mutex_init(&mutex_io, NULL);
-    //busco el índice del planificador
-    int scheduler = indexSched(argv[1]);
-    /*
-    **  dependiendo del tipo de planificador se aplica una función
-    **  si no coincide con ninguno de los planificadores definidos
-    **  se imprime la ayuda
-    */
-    switch (scheduler)
-    {
-        case 0:
-            fcfs();
-            break;
-        case 1:
-            sjf();
-            break;
-        case 2:
-            rr();
-            break;
-        default:
+    long q;
+    //Verifico que se hayan ingresado correctamente los argumentos al comando, caso contrario, no hago ni guardo nada.
+    if (argv[1]==NULL){
+        printf ("\n[ERROR] Archivo no definido\n");
+        printHelp();
+        exit(EXIT_FAILURE);
+    }
+    if(argv[2]==NULL){
+        printf ("\n[ERROR] Planificador no definido\n");
+        printHelp();
+        exit(EXIT_FAILURE);
+    }
+    if(strcmp("rr", argv[2]) == 0){
+        if(argv[3]==NULL){
+            printf ("\n[ERROR] No se definió se definió el quantum para round robin.\n");
             printHelp();
-            break;
+            exit(EXIT_FAILURE);
+        }
+        else{
+            char *ptr;
+            q = strtol(argv[3], &ptr, 10);
+            if(q<=0 || q==NULL){
+                printf ("\n[ERROR] Quantum definido en argumento no es un número. Cambielo por un número y vuelva a intentar.\n");
+                printHelp();
+                exit(EXIT_FAILURE);
+            }
+        }
+    }
+    
+    //aqui si empiezo a definir variables
+    pthread_mutex_init(&mutex_io, NULL); // defino mutex para io
+    char * file_path = argv[1]; // defino ruta del archivo
+    int scheduler = indexSched(argv[2]); //tipo de sched//busco el índice del planificador
+    if(fillProcessQueues(file_path)){
+        /*
+        **  Dependiendo del tipo de planificador se aplica una función
+        **  si no coincide con ninguno de los planificadores definidos
+        **  se imprime la ayuda
+        */
+        switch (scheduler)
+        {
+            case 0:
+                fcfs();
+                break;
+            case 1:
+                sjf();
+                break;
+            case 2:
+                rr(argv[3]);
+                break;
+            default:
+                printHelp();
+                break;
+        }
+    }
+    else{
+        printf ("\n[ERROR] Error con el archivo. Asegurese de haber escrito correctamente la ruta\n");
+        printHelp();
+        exit(EXIT_FAILURE);
     }
     return 0;
 }
@@ -56,13 +97,13 @@ void printHelp()
     printf("\n Autor: Daniela Montenegro");
     printf("\n Sistemas Operativos 2020-1S\n");
     printf("\n Uso: \n");
-    printf("\n ./schedsim [planificador:String] (opcional: [quantum:Integer] //solo para round robin//)\n");
+    printf("\n ./schedsim [schedtimes.dat - path:String] [planificador:String] (opcional: [quantum:Integer] //solo para round robin//)\n");
     printf("\n First Come First Served: ");
-    printf("\n ./schedsim fcfs");
+    printf("\n ./schedsim schedtimes.dat fcfs");
     printf("\n Shortest Job First (Apropiativo): ");
-    printf("\n ./schedsim sjf");
+    printf("\n ./schedsim schedtimes.dat sjf");
     printf("\n Round Robin: ");
-    printf("\n ./schedsim rr [quantum:Integer]\n");
+    printf("\n ./schedsim schedtimes.dat rr [quantum:Integer]\n");
     printf("\n ************************************************************************************\n");    
     pthread_mutex_unlock (&mutex_io);
     return;
@@ -84,4 +125,19 @@ void sjf(){
 }
 void fcfs(){
     printf("\nFirst Come First Served Scheduler\n");
+}
+bool fillProcessQueues(char * file_path){
+    int llegada,rafaga;//variables temporales para almacenar los digitos del archivo cuando se lea linea por linea
+    int index=0;//número de línea
+    FILE *fp; //se define el archivo con la data de procesos 
+    fp = fopen(file_path, "r");//archivo
+    if (fp == NULL){
+        return false;
+    }
+    while(fscanf(fp, "%d %d", &llegada, &rafaga)!= EOF){
+        index++;
+        printf("\n[INFO] Leyendo línea %d -- Llegada: %d Ráfaga: %d\n",index,llegada,rafaga); //mensaje informativo
+    }    
+    fclose(fp);
+    return true;
 }
